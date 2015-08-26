0417df01585002dbf7c9f6e79f591115894f0271
huntinux <huntinux@gmail.com>
2015-08-26 15:37:17 +0800

    using sigaction

--- 30chp/serv02.c
+++ 30chp/serv02.c
@@ -23,6 +23,29 @@
 #define MAXLINE 1024
 #define	MAXN	16384		/* max # bytes client can request */
 
+typedef void (Sigfunc)(int);
+Sigfunc*  signal(int signo, Sigfunc *func)
+{
+	struct sigaction	act, oact;
+
+	act.sa_handler = func;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	if (signo == SIGALRM) {
+#ifdef	SA_INTERRUPT
+		act.sa_flags |= SA_INTERRUPT;	/* SunOS 4.x */
+#endif
+	} else {
+#ifdef	SA_RESTART
+		act.sa_flags |= SA_RESTART;		/* SVR4, 44BSD */
+#endif
+	}
+	if (sigaction(signo, &act, &oact) < 0)
+		return(SIG_ERR);
+	return(oact.sa_handler);
+}
+
+
 pid_t child_make(int i, int listenfd, int addrlen)
 {
 	pid_t	pid;
